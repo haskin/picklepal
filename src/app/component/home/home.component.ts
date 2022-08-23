f6972623175381626778588b440b60240ad82f57
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-23 17:11:02 -0400

    changed swipe function names

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -9,44 +9,48 @@ import { ProfileService } from 'src/app/service/profile.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   title = 'Pickle Pal';
   index: number = 0;
   profiles: Profile[] = profileData;
   pals: Set<number> = new Set<number>();
   skillLevel: SkillLevel = SkillLevel.Beginner;
+  cardSwipe: string = '';
   // pals: Set<string> = new Set<string>;
-  increaseIndex() {
-    console.log("clicked arrow");
+  leftSwipe() {
+    console.log('clicked arrow');
     this.index = (this.index + 1) % this.profiles.length;
   }
-  addPal() {
+  rightSwipe() {
     this.palService.addPal(this.index);
     this.index = (this.index + 1) % this.profiles.length;
   }
 
   // constructor injection
-  constructor(private palService: PalService, private profileService: ProfileService) {
+  constructor(
+    private palService: PalService,
+    private profileService: ProfileService
+  ) {
     this.pals = palService.getPals();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   filterBySkillLevel(skillLevelFilter: SkillLevel) {
     this.skillLevel = skillLevelFilter;
     // console.log(this.skillLevel);
     this.filter();
-    
-    //profileData = profileData.filter() 
+
+    //profileData = profileData.filter()
   }
 
   filter() {
     let allProfiles = this.profileService.getProfiles();
-    this.profiles = allProfiles.filter( profile => profile.skillLevel == this.skillLevel);
+    this.profiles = allProfiles.filter(
+      (profile) => profile.skillLevel == this.skillLevel
+    );
   }
 }
 
